2978feed221579049d94a3d82ff9e358b788a94c
ikram-12 <62016836+ikram-12@users.noreply.github.com>
2021-07-13 17:41:25 +0200

    modified

--- App_Dash1-master/apps/test_page2.py
+++ App_Dash1-master/apps/test_page2.py
@@ -16,11 +16,14 @@ db = mysql.connect(
     database="morroco1",
     auth_plugin='mysql_native_password',
 )
-df8 = pd.read_sql(
-    "select distinct(morocco_04_18.dim_region.Region), morocco_04_18.fact_effectifs.EffectifsP_R ,morocco_04_18.fact_effectifs.EvolutionP_R, morocco_04_18.dim_year.Year from morocco_04_18.dim_region, morocco_04_18.fact_effectifs, morocco_04_18.dim_year where morocco_04_18.fact_effectifs.Id_region = morocco_04_18.dim_region.Id_region and morocco_04_18.fact_effectifs.Id_year = morocco_04_18.dim_year.Id_year",con=db)
+# df8 = pd.read_sql(
+#     "select distinct(morocco_04_18.dim_region.Region), morocco_04_18.fact_effectifs.EffectifsP_R ,morocco_04_18.fact_effectifs.EvolutionP_R, morocco_04_18.dim_year.Year from morocco_04_18.dim_region, morocco_04_18.fact_effectifs, morocco_04_18.dim_year where morocco_04_18.fact_effectifs.Id_region = morocco_04_18.dim_region.Id_region and morocco_04_18.fact_effectifs.Id_year = morocco_04_18.dim_year.Id_year",con=db)
 
-# fig2 = ff.create_table(df8[['Region','EvolutionP_R','Year']], height_constant=60)
+df8 = pd.read_sql(
+    "select distinct(morocco_04_18.dim_region.Region), morocco_04_18.fact_effectifs.EffectifsP_R ,morocco_04_18.fact_effectifs.EffectifsSC_R ,morocco_04_18.fact_effectifs.EffectifsSQ_R ,morocco_04_18.fact_effectifs.EvolutionP_R,morocco_04_18.fact_effectifs.EvolutionSC_R,morocco_04_18.fact_effectifs.EvolutionSQ_R, morocco_04_18.dim_year.Year from morocco_04_18.dim_region, morocco_04_18.fact_effectifs, morocco_04_18.dim_year where morocco_04_18.fact_effectifs.Id_region = morocco_04_18.dim_region.Id_region and morocco_04_18.fact_effectifs.Id_year = morocco_04_18.dim_year.Id_year",con=db)
+
 all_years = df8["Year"].unique()
+a=df8[[ 'EvolutionP_R','EvolutionSC_R','EvolutionSQ_R']].head(0)
 
 # ---------------------------------------------------------------
 layout = html.Div([
@@ -33,18 +36,28 @@ layout = html.Div([
                      for x in all_years],
             value=all_years[3:], ),
     ]),
+    html.Div([
+        html.Label(['test']),
+        html.P("education level:"),
+        dcc.Dropdown(
+            id='el', style={'height': '50px', 'width': '200px'},
+            options=[{'value': x, 'label': x}
+                     for x in a.columns],
+            value=a.columns[-1:], ),
+    ]),
 
     dcc.Graph(id='t1',figure={}),
 ])
 # ---------------------------------------------------------------
 @app.callback(
     Output(component_id='t1', component_property='figure'),
-    [Input(component_id='years', component_property='value')])
-def update_bar_chart(year):
-    df = df8[['Region','EvolutionP_R','Year']]
+    [Input(component_id='years', component_property='value'),
+     Input(component_id='el', component_property='value')])
+def update_bar_chart(year,edlev):
+    df = df8[['Region',edlev,'Year']]
     mask = df["Year"] == year
     df3 = df[mask]
-    df4 = df3[['Region', 'EvolutionP_R']]
+    df4 = df3[['Region', edlev]]
     fig2 = ff.create_table(df4, height_constant=27)
     fig2.layout.width = 600
     return fig2
